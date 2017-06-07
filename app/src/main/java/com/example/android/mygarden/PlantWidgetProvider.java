14e7b38bd39c32186f60ffa38856eac0f3870f7b
MaximBravo <maxim@alexbravo.com>
2017-06-07 15:19:49 -0700

    try to solve update widget service TWID0.3

---
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -0,0 +1,58 @@
+package com.example.android.mygarden;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class PlantWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int imgRes,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.app_name);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(
+                context,
+                0,
+                wateringIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(imgRes, wateringPendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        PlantWateringService.startActionUpdatePlantWidgets(context);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager, int imgRes, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, imgRes, appWidgetId);
+        }
+    }
+}
+
